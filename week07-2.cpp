9b17a154d841625b7d225cf85915b1a46cea5c71
Yumao <91170805+Yumao0605@users.noreply.github.com>
2021-10-27 12:04:11 +0800

    Add files via upload

---
+++ week07-2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int n=10;
+    while (n>0)
+    {
+        printf("n¤j©ó0\n");
+    }
+}
---
+++ week07-6.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    for (int i=0;i<4;i++)
+    {
+        printf("i:%d\n",i);
+    }
+
+    for (int i=1;i<=4;i++)
+    {
+        printf("人類的數法i:%d\n",i);
+    }
+    for (int i=0;i<=4;i++)
+    {
+     printf("怪怪的..%d\n",i);
+    }
+}
