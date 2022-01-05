5dd0631b5e8ac05e62ed1c510af532e2ee7479ad
Yumao <91170805+Yumao0605@users.noreply.github.com>
2022-01-05 11:34:22 +0800

    Add files via upload

---
+++ week03/week03-5.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello%dWorld\n", 300);
+    printf("%d %d    %d", 300, 200, 100);
+}
+
---
+++ week03/week03-6.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("請輸入一個整數a的值:");
+    scanf("%d", &a);
+
+    printf("a的值是:%d\n", a);
+}
